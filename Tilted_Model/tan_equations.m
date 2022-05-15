d55af736fac862e586b01b621245076493f9a150
matthieukohl <98239886+matthieukohl@users.noreply.github.com>
2022-05-15 14:45:59 -0400

    test_delete

--- Tilted_Model/tan_equations.m
+++
@@ -1,9 +0,0 @@
-function [RHS1,RHS2] = tan_equations(k1,k2,sigma,b,r)
-
-% Calculate the two tangent equations
-
-RHS1 = tan(k1.*b)+r.*k1.*k2./(1+sigma).*(1./(r.*k2)-sigma.*k2./(sigma.^2+r-1));
-
-RHS2 = tan(k2.*b)-r.*k1.*k2./(1+sigma).*(-1./(r.*k1)+sigma.*k1./(sigma.^2+r-1));
-
-end
